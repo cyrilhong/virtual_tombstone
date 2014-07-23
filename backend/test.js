df3777d65cf9f677fbf420160e592a5f67db1f05
edenyeh <eden_yeh@livemail.tw>
2014-07-23 14:25:08 +0800

    eden fb id

--- backend/test.js
+++ backend/test.js
@@ -16,8 +16,8 @@ db.open(function(err, db) {
       } else {
 
         var vts = [
-          {_id:"steve_jobs", owner_id:'53c29ffd5d87316b1612e42e',  vtPhoto:'', vtName:'STEVE JOBS', vtDes:'Stewart and his team put out several issues of The Whole Earth Catalog, and then when it had run its course, they put out a final issue. It was the mid-1970s, and I was your age. On the back cover of their final issue was.', vtDate: '12.06.2005', vtLike: 12, vtStare: 34, vtMsg: 5},
-          {_id:"charlie_chaplin", owner_id:'azole',  vtPhoto:'', vtName:'CHARLIE CHAPLIN', vtDes:'Sir Charles Spencer "Charlie" Chaplin, KBE (16 April 1889 - 25 December 1977) was an English actor, comedian, and filmmaker, who rose to fame in the silent era.', vtDate: '25.12.1977', vtLike: 56, vtStare: 34, vtMsg: 10}
+          {_id:"steve_jobs", owner_id:'53c901e33ee7aa2411c5753f',  vtPhoto:'', vtName:'STEVE JOBS', vtDes:'Stewart and his team put out several issues of The Whole Earth Catalog, and then when it had run its course, they put out a final issue. It was the mid-1970s, and I was your age. On the back cover of their final issue was.', vtDate: '12.06.2005', vtLike: 12, vtStare: 34, vtMsg: 5},
+          {_id:"charlie_chaplin", owner_id:'53c901e33ee7aa2411c5753f',  vtPhoto:'', vtName:'CHARLIE CHAPLIN', vtDes:'Sir Charles Spencer "Charlie" Chaplin, KBE (16 April 1889 - 25 December 1977) was an English actor, comedian, and filmmaker, who rose to fame in the silent era.', vtDate: '25.12.1977', vtLike: 56, vtStare: 34, vtMsg: 10}
         ];
 
         db.collection('vts', function(err, collection) {
