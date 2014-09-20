487ce2310231ceca0c12e6c5d9204693f9e71746
root <root@localhost.localdomain>
2014-09-21 00:43:39 +0800

    for delete tool

--- backend/delete.js
+++ backend/delete.js
@@ -12,7 +12,7 @@ var server = new Server('localhost', 27017, {
 });
 var db = new Db('vtdb', server);
 db.open(function(err, db) {
-  var id = '541da960f85de03a49f783d3';
+  var id = '541da337f85de03a49f783ce';
   db.collection('msgs', function(err, collection) {
     collection.remove({
       '_id': new BSON.ObjectID(id)
