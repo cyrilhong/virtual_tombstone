dfe3a25c25f17aebec3b57b93fa024269cafee54
Cyril Hong <cyrilhong@gmail.com>
2014-09-22 19:17:34 +0800

    update css

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ var passport = require('passport'),
 var FACEBOOK_APP_ID = "781476481883407";
 var FACEBOOK_APP_SECRET = "5f42a96dd2950b4be69ce0fcf8dd69c8";
 var FACEBOOK_CALLBACK_URI = "http://virtualtombstone.co/auth/facebook/callback";
-// var FACEBOOK_APP_ID = "720220748043210";
-// var FACEBOOK_APP_SECRET = "5f126a750c9e5d9a53972d394af4aec0";
+// var FACEBOOK_APP_ID = "754983211225886";
+// var FACEBOOK_APP_SECRET = "593acf13a4ae2cbdc176e8763a3cc74b";
 // var FACEBOOK_CALLBACK_URI = "http://localhost:3000/auth/facebook/callback";
 
 
