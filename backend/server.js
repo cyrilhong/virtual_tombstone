cc674de6ad4bcf28fc3e5d17912f8624bed00e4c
Cyril Hong <cyrilhong@gmail.com>
2014-09-23 20:27:00 +0800

    css update

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,7 @@ app.use('/test/',express.static(__dirname+'/vt_test'));
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/www')); // Eden 開發用資料夾
 
-// app.listen(80);
-// console.log('Listening on port 80...');
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(80);
+console.log('Listening on port 80...');
+// app.listen(3000);
+// console.log('Listening on port 3000...');
