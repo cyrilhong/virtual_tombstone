e3abf2e342bfa0524d09ed94fa016c2177975da4
azole <azolelai@gmail.com>
2014-10-04 23:28:07 +0800

    redirect old domain name

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@
 'use strict';
 
 var express = require('express'),
-    vts = require('./routes/vts'),
-    users = require('./routes/users');
+  vts = require('./routes/vts'),
+  users = require('./routes/users');
 
 var passport = require('passport'),
-    FacebookStrategy = require('passport-facebook').Strategy;
+  FacebookStrategy = require('passport-facebook').Strategy;
 
 // server //
 var FACEBOOK_APP_ID = "781476481883407";
@@ -35,15 +35,17 @@ passport.use(new FacebookStrategy({
     users.findOneOrCreateUser(accessToken, profile, done);
   }
 ));
- 
+
 var app = express();
- 
-app.configure(function () {
-  app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-  app.use(express.cookieParser());  // for fb login
+
+app.configure(function() {
+  app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+  app.use(express.cookieParser()); // for fb login
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.session({ secret: 'virtual_tombstone' }));
+  app.use(express.session({
+    secret: 'virtual_tombstone'
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
   //app.use(express.bodyParser({ uploadDir: __dirname + '/../www/img/face' }));
@@ -59,10 +61,17 @@ passport.deserializeUser(function(id, done) {
   users.findUserById(id, done);
 });
 
+// redirect
+app.get('/*', function(req, res, next) {
+  var isOld = req.headers.host.match(/virtual_tombstone/),
+    url = ['http://virtualmonument.co', req.url].join('');
+  console.log("******redirect", url);
+  isOld ? next() : res.redirect(301, url);
+});
 // routes
 app.get('/vts', vts.findAll); // retrieve all virtual tombstones
-app.get('/vts/:id', vts.findById);  // retrieve the virtual tombstone by id
-app.put('/vts/:id', vts.updateVt);  // update virtual tombstone by id
+app.get('/vts/:id', vts.findById); // retrieve the virtual tombstone by id
+app.put('/vts/:id', vts.updateVt); // update virtual tombstone by id
 app.delete('/vts/:id', vts.deleteVt); // delete virtual tombstone by id
 //app.options('/vts/:id', vts.deleteVt);  // for jquery test
 
@@ -71,15 +80,17 @@ app.post('/vts/:id/msgs', ensureAuthenticated, vts.addMessage);
 
 app.get('/user', users.getUser);
 app.get('/user/:id/vts', users.findVtsByUser);
-app.post('/user/:id/vts', ensureAuthenticated, users.addVt);  // add vts for user
+app.post('/user/:id/vts', ensureAuthenticated, users.addVt); // add vts for user
 
-app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'public_profile','user_friends', 'read_friendlists', 'publish_actions']}));
+app.get('/auth/facebook', passport.authenticate('facebook', {
+  scope: ['email', 'public_profile', 'user_friends', 'read_friendlists', 'publish_actions']
+}));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/',
   failureRedirect: '/login-failed.html'
 }));
 
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
@@ -91,8 +102,10 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/');
 }
 
-app.all(/^\/test$/, function(req, res) { res.redirect('/test/'); });
-app.use('/test/',express.static(__dirname+'/vt_test'));
+app.all(/^\/test$/, function(req, res) {
+  res.redirect('/test/');
+});
+app.use('/test/', express.static(__dirname + '/vt_test'));
 
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/www')); // Eden 開發用資料夾
