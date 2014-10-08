95d5b3b75f685bca26fa1a64c6bd9ee85afbe058
Cyril Hong <cyrilhong@gmail.com>
2014-10-08 23:47:51 +0800

    delete 1U49C30X6UW0

--- backend/delete.js
+++ backend/delete.js
@@ -12,7 +12,7 @@ var server = new Server('localhost', 27017, {
 });
 var db = new Db('vtdb', server);
 db.open(function(err, db) {
-  var id = '5424d498f85de03a49f78566';
+  var id = '5424d297f85de03a49f78563';
   db.collection('vts', function(err, collection) {
     collection.remove({
       '_id': new BSON.ObjectID(id)
